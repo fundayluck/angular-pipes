6e13e45e1f6dd14701c3d777d43b1bbf6c8db83d
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-08 05:34:24 +0700

    adding payment amount form and displaying with currencyPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TitleCasePipe, DatePipe } from '@angular/common';
+import { TitleCasePipe, DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, DatePipe],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name?: string
   date?: string
+  amount?: number
 
   onNameChange(event: Event) {
     this.name = (event?.target as HTMLInputElement).value
@@ -19,5 +20,9 @@ export class AppComponent {
 
   onDateChange(event: Event) {
     this.date = (event?.target as HTMLInputElement).value
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = parseFloat((event?.target as HTMLInputElement).value)
   }
 }
