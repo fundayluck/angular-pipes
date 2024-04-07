bda693e619d614c05b65ad144b992201e4a35aaf
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-08 06:21:17 +0700

    implement custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TitleCasePipe, DatePipe, CurrencyPipe, DecimalPipe, JsonPipe } from '@angular/common';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, DecimalPipe, JsonPipe],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, DecimalPipe, JsonPipe, ConvertPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,11 +15,16 @@ export class AppComponent {
   date?: string
   amount?: number
   height?: number
+  miles?: number
 
   car = {
     make: 'Toyota',
     model: 'Camry',
     year: 2010
+  }
+
+  onMilesChange(event: Event) {
+    this.miles = parseFloat((event?.target as HTMLInputElement).value)
   }
 
   onHeightChange(event: Event) {
--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -6,8 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ...args: unknown[]): unknown {
+    if (!value) {
+      return ''
+    }
+
+    return value * 1.60934;
   }
 
 }
