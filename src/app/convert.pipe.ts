fe058d43e3f5f1fecb8607f78cdd7e133c5cf817
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-08 06:59:48 +0700

    example custom arguments

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -6,12 +6,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any, target: string): unknown {
     if (!value) {
       return ''
     }
 
-    return value * 1.60934;
+    switch (target) {
+      case 'km':
+        return value * 1.60934
+      case 'm':
+        return value * 1.60934 * 1000
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000
+      default:
+        return "Here we go again!";
+    }
   }
 
 }
